3027e96f4574e6a3bfc940777dd906b87d5b9623
wiKanok <kanokwan1401.ks@gmail.com>
2024-05-10 13:00:24 +0700

    feat: objects ex-1

--- ex-1.js
+++ ex-1.js
@@ -1,3 +1,22 @@
 // Exercise #1: Food Order
 
 // Start coding here
+const foodOrder = {
+  orderNumber: "A0234",
+  address: "Bangkok",
+  paymentType: "Cash",
+  totalPrice: 4500,
+  restaurantName: "MK",
+};
+
+foodOrder.totalPrice = 5000;
+foodOrder.paymentType = "Credit Card";
+
+console.log(foodOrder); // =>
+// {
+//   orderNumber: 'A0234',
+//   address: 'Bangkok',
+//   paymentType: 'Credit Card',
+//   totalPrice: 5000,
+//   restaurantName: 'MK'
+// }
